eb0fcf909e3e7cda179a2179881682d109149ee7
ilès <iles@mentorgoal.com>
2025-03-07 14:24:38 +0100

    create ennemy class

---
+++ Assets/Scripts/Ennemy.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.AI
+
+public class EnemyEntity: Monobehavior{
+    public NavMeshAgent agent;
+    public TransForm player;
+    public LayerMask isGround,isPlayer 
+
+    public vector3 walkPoint
+
+}
+
