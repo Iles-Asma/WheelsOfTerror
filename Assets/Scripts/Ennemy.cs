5c003b84d4238e5171e3e60288d8527b22cee46d
ilès <iles@mentorgoal.com>
2025-04-09 22:47:32 +0200

    add object

--- Assets/Scripts/Ennemy.cs
+++ Assets/Scripts/Ennemy.cs
@@ -4,7 +4,7 @@ using UnityEngine.AI
 public class EnemyEntity: Monobehavior{
     public NavMeshAgent agent;
     public TransForm player;
-    public LayerMask isGround,isPlayer 
+    public LayerMask isGround,isPlayer
 
     public vector3 walkPoint
 
