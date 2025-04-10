c554ee40f66a658b1e9048aac3244ac3f6c15b6c
ilès <iles@mentorgoal.com>
2025-04-10 09:39:16 +0200

    fix camera, rigidbody, collider_mesh

--- Assets/Scripts/Ennemy.cs
+++ Assets/Scripts/Ennemy.cs
@@ -1,15 +1,15 @@
-using UnityEngine;
-using UnityEngine.AI
+// using UnityEngine;
+// using UnityEngine.AI
 
-public class EnemyEntity: Monobehavior{
-    public NavMeshAgent agent;
-    public TransForm player;
-    public LayerMask isGround,isPlayer
+// public class EnemyEntity: Monobehavior{
+//     public NavMeshAgent agent;
+//     public TransForm player;
+//     public LayerMask isGround,isPlayer
 
-    public vector3 walkPoint
+//     public vector3 walkPoint
 
-    public float timebetween
+//     public float timebetween
 
 
-}
+// }
 
