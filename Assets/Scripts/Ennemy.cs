82b73e74e9bbf52e908937b1740edb93f84b7850
Iles-Asma <iles.a@icloud.com>
2025-04-11 05:37:32 +0200

    adding ennemy IA to follow player

--- Assets/Scripts/Ennemy.cs
+++ Assets/Scripts/Ennemy.cs
@@ -1,15 +1,42 @@
-// using UnityEngine;
-// using UnityEngine.AI
+using UnityEngine;
+using UnityEngine.AI
 
-// public class EnemyEntity: Monobehavior{
-//     public NavMeshAgent agent;
-//     public TransForm player;
-//     public LayerMask isGround,isPlayer
+ public class EnemyAI : Monobehavior
+{
+    public NavMeshAgent agent;
+    public TransForm player; // Reference du joueur
+    public float detectRange = 30f; // distance a laquelle l'ennemie suie le joueur
+    public float stopDistance = 2f; // distance a laquelle l'ennemie s'arrête
+    public float updateRate = 0.5f; // fréquence de mise à jour de la détection
 
-//     public vector3 walkPoint
+    public NavMeshAgent agent;
+    private float nextUpdateTime = 0f; // gestion de mis a jour du temps
 
-//     public float timebetween
+    void Start() { 
+        agent = GetComponent<NavMeshAgent>();
+
+    }
+
+    void Update()
+    {
+        float distanceToPlayer = Vector3.Distance(player.position, transform.position);
+
+        if (distanceToPlayer < detectRange)
+        {
+            // delai avant de recalculer la destination
+            nextUpdateTime -= Time.deltaTime;
+            if (nextUpdateTime <= 0f)
+            {
+                // mis a jour de la destination de l'ennemi et reinitialisation du timer
+                agent.SetDestination(player.position);
+                nextUpdateTime = updateRate;
+            }
+
+        }
+    }
 
 
-// }
+
+
+}
 
